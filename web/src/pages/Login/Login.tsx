b4b98475b2e924a32f6d78a62eb8274bb3dcae97
Pablo Luan <pablo-544@hotmail.com>
2025-04-09 01:17:15 -0300

    feat: updating inputs fields and create api

--- web/src/pages/Login/Login.tsx
+++ web/src/pages/Login/Login.tsx
@@ -1,32 +1,66 @@
 import * as S from './styles.ts'
 import banner from '../../../public/login-banner.jpg'
-import { Button, Container, Input, InputLabel } from '@mui/material'
+import { Box, Button, Container, TextField, Typography } from '@mui/material'
+import React from 'react'
+import { api } from '../../lib/api.ts'
 
 export default function Login() {
+
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+  
+    const handleSubmit = async (event: React.FormEvent) => {
+      event.preventDefault()
+      console.log({ username, password })
+      // Aqui você pode fazer login ou validação
+
+      const response = await api.get('/diagnostics')
+      console.log(response)
+    //   try {
+    //     }
+    //     catch (error) {
+    //         console.log(error)
+    //     }
+
+    }
+
+
     return(
         <S.Container>
             <S.FormContainer>
-            
-
-
-
-
-
-                <form>
-                    <Container>
-                        <InputLabel htmlFor="username">User</InputLabel>
-                        <Input id="username" name="username" fullWidth required />
-                    </Container>
-                    <Container>
-                        <InputLabel htmlFor="password">Senha</InputLabel>
-                        <Input id="password" name="password" type="password" fullWidth required />
-                    </Container>
-                    <Container>
-                        <Button variant="contained" color="primary" type="submit">
-                            Login
+                <Container maxWidth="sm">
+                    <Typography variant="h5" component="h1" textAlign={'center'} gutterBottom>
+                        Bem vindo!
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit}>
+                        <TextField
+                            label="Username"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            sx={{ marginTop: 2 }}
+                        >
+                            Entrar
                         </Button>
-                    </Container>
-                </form>
+                    </Box>
+                </Container>
             </S.FormContainer>
             <S.Container className=''>
                 <S.Image src={banner} alt="Banner login" />
