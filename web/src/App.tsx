05d1b55a5bd56977f452113843dc8b6da4ca1282
Pablo Luan <pablo-544@hotmail.com>
2025-04-11 01:48:26 -0300

    feat: including a chart route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login/Login'
-import Dashboard from './pages/Login/Dashboard'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Chart from './pages/Chart/Chart'
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} index />
+        <Route path="/chart" element={<Chart />}  />
       </Routes>
     </Router>
   )
---
+++ web/src/pages/Chart/Chart.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+
+export default function Chart() {
+    return (
+        <div>
+
+
+        <Container>
+            <h1>Chart</h1>
+            <p>Chart Page</p>
+        </Container>
+        </div>
+    )
+}
